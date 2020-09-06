aa22416437affd775fd8d083675a4f444937125d
Rodrigo Lins e Freitas <rlinsdev@gmail.com>
2020-09-06 17:40:06 -0300

    cookie-parser and samples of cookie

--- app.js
+++ app.js
@@ -1,18 +1,23 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const authRoutes = require("./routes/authRoutes");
+const cookieParser = require("cookie-parser");
 
 const app = express();
 
 // middleware
 app.use(express.static("public"));
+app.use(express.json());
+app.use(cookieParser());
 
 // view engine
 app.set("view engine", "ejs");
 
 // database connection
 const dbURI =
-  "mongodb+srv://gqluser:teste**@cluster0.itrkd.mongodb.net/graphiql?retryWrites=true&w=majority";
-mongoose.connect(dbURI, {
+  "mongodb+srv://dbuser:123@auth.t8yit.mongodb.net/node-auth";
+mongoose
+  .connect(dbURI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
@@ -23,3 +28,24 @@ mongoose.connect(dbURI, {
 // routes
 app.get("/", (req, res) => res.render("home"));
 app.get("/smoothies", (req, res) => res.render("smoothies"));
+app.use(authRoutes);
+
+// Cookies
+app.get("/set-cookies", (req, res) => {
+  // res.setHeader('Set-Cookie','newUser=true');
+
+  res.cookie("newUser", false);
+  res.cookie("isEmployee", true, {
+    maxAge: 1000 * 60 * 20, // Will expire after 20"
+    secure: false, // HTTP Access. Change to secure: true
+    httpOnly: true, // Java script will not able to access
+  });
+
+  res.send("you got the cookies!");
+});
+
+app.get("/read-cookies", (req, res) => {
+  const cookies = req.cookies;
+  console.log(cookies);
+  res.json(cookies);
+});
