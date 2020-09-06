e32a543f61d97e0daccb8de63f461b8db34290a2
Rodrigo Lins e Freitas <rlinsdev@gmail.com>
2020-09-06 16:22:23 -0300

    Mongoose Hooks

--- models/User.js
+++ models/User.js
@@ -1,21 +1,35 @@
-const mongoose = require('mongoose');
-const { isEmail } = require('validator');
+const mongoose = require("mongoose");
+const { isEmail } = require("validator");
 
 const userSchema = new mongoose.Schema({
   email: {
     type: String,
-    required: [true, 'Please enter an email'],
+    required: [true, "Please enter an email"],
     unique: true,
     lowercase: true,
-    validate: [isEmail, 'Please enter a valid email']
+    validate: [isEmail, "Please enter a valid email"],
   },
   password: {
     type: String,
-    required: [true, 'Please enter a password'],
-    minlength: [6, 'Minimum password length is 6 characters'],
-  }
+    required: [true, "Please enter a password"],
+    minlength: [6, "Minimum password length is 6 characters"],
+  },
 });
 
-const User = mongoose.model('user', userSchema);
+// fire a function after do saved to db
+// Hook and/or middleware
+userSchema.post("save", function (doc, next) {
+  console.log("new user was created % saved", doc);
+  next();
+});
+
+// fire a function before doc sabed to BD
+userSchema.pre('save', function(next){
+  console.log('user about to be created & saved',this);
+  next();
+
+});
+
+const User = mongoose.model("user", userSchema);
 
 module.exports = User;
