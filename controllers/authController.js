705137d62c4d0fc926201bf6f70f1d95a35e98f5
Rodrigo Lins e Freitas <rlinsdev@gmail.com>
2020-09-06 21:04:42 -0300

    signup controle

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,5 @@
 const User = require("../models/User");
+const jwt = require("jsonwebtoken");
 
 // handle errors
 const handleErrors = (err) => {
@@ -24,6 +25,15 @@ const handleErrors = (err) => {
   return errors;
 };
 
+//const maxAge=  3*24*60*60; // 3 days 24 h 60 min 60 second
+const maxAge = 20 * 60; // 3 days 24 h 60 min 60 second
+
+const createToken = (id) => {
+  return jwt.sign({ id }, "net ninja secret", {
+    expiresIn: maxAge,
+  });
+};
+
 // controller actions
 module.exports.signup_get = (req, res) => {
   res.render("signup");
@@ -38,7 +48,9 @@ module.exports.signup_post = async (req, res) => {
 
   try {
     const user = await User.create({ email, password });
-    res.status(201).json(user);
+    const token = createToken(user._id);
+    res.cookie("jwt", token, { httpOnly: true, maxAge: maxAge * 1000 });
+    res.status(201).json({user: user._id});
   } catch (err) {
     const errors = handleErrors(err);
     res.status(400).json({ errors });
